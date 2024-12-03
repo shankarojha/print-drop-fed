0d3fb7c3176f4afaefa71a6980aae85c04f89fe8
Shankar Ojha <shankarojha34@gmail.com>
2024-12-03 11:51:59 +0530

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"signup",
+        //component:SignupComponent
+        loadComponent:()=> import("./signup/signup.component").then((c)=>c.SignupComponent)
+    },
+    {
+        path:"dashboard",
+       // component:DashboardComponent
+       //lazy loading
+       loadComponent:()=> import("./dashboard/dashboard.component").then((c)=>c.DashboardComponent)
+    }
+];
