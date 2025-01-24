9a8b4d891625907f72e16cc6207f571940b2f501
Shankar Ojha <shankarojha34@gmail.com>
2025-01-25 01:40:31 +0530

    signup

--- src/app/features/signup/signup.component.ts
+++ src/app/features/signup/signup.component.ts
@@ -1,11 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule,MatFormFieldModule, MatInputModule, MatIconModule],
   templateUrl: './signup.component.html',
-  styleUrl: './signup.component.css'
+  styleUrl: './signup.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class SignupComponent {
+
+  /** defining reactive signup forms */
+
+  signUpForm = new FormGroup({
+    name: new FormControl('', [Validators.required]),
+    email: new FormControl('', [Validators.email]),
+    phoneNumber: new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(10)]),
+    country: new FormControl("India", Validators.required),
+    city: new FormControl('', [Validators.required]),
+    address: new FormControl('', [Validators.required]),
+    userType: new FormControl('', [Validators.required]),
+    password: new FormControl('', [Validators.required, Validators.minLength(8), Validators.pattern(/^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[\W_]).{8,}$/)]),
+  })
+
+  onSignupSubmit(){
+    console.log(this.signUpForm.value)
+  }
+
+  /**Ends */
 
 }
